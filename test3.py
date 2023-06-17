4872d1e3a1215be574146bf4efab3c73277ae8a9
Khadijeh Valipour <khadijehvalipour7777@gmail.com>
2023-06-17 17:32:14 +0330

    dovomin commite man

--- test3.py
+++ test3.py
@@ -1,1 +1,12 @@
-print("hello world")
+
+import csv
+from statistics import mean
+with open('grades.csv') as f :
+    reader= csv.reader(f)
+    for row in reader:
+        print(row)
+        name=row[0]
+        these_grades=list()
+        for grade in row[1:] :
+            these_grades.append(int(grade))
+        print("average of %9s is %.2f" %(name ,mean(these_grades)))
